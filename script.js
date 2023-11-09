68609b9e06f9ea8031afa8897c7e52bb4e180b97
Gannon <gannon.barnett+mac@gmail.com>
2023-11-08 16:29:14 -0800

    Redirect if no client ID

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 console.log('Hello World');
 
-fetch("http://starlinkrouter/").then((response) => {
-    console.log(response);
-    document.getElementById("response").innerHTML = response;
-});
+// This request has been blocked; the content must be served over HTTPS.
+// fetch("http://starlinkrouter/").then((response) => {
+//     console.log(response);
+//     document.getElementById("response").innerHTML = response;
+// });
+
+function param(name) {
+    return (location.search.split(name + '=')[1] || '').split('&')[0];
+}
+
+function set_output(value) {
+    document.getElementById("output").innerHTML = value;
+}
+
+window.onload = function() {
+    const client_id = param("client_id");
+    if (!client_id) {
+        set_output("<i>Missing client_id</i>");
+        window.location.href = "http://starlinkrouter/";
+    } else {
+        set_output(client_id);
+    }
+}
